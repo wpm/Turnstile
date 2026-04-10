8bc276845e95f065382541133df91076ef3c2b52
W.P. McNeill <billmcn@gmail.com>
2026-04-10 02:44:13 -0500

    feat: Change to Turnstile icon

--- src/app.rs
+++ src/app.rs
@@ -45,7 +45,7 @@ pub fn App() -> impl IntoView {
 
             <div class="row">
                 <a href="https://tauri.app" target="_blank">
-                    <img src="public/tauri.svg" class="logo tauri" alt="Tauri logo"/>
+                    <img src="public/turnstile.svg" class="logo tauri" alt="Turnstile logo"/>
                 </a>
                 <a href="https://docs.rs/leptos/" target="_blank">
                     <img src="public/leptos.svg" class="logo leptos" alt="Leptos logo"/>
